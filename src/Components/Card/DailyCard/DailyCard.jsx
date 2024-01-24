7f6b589fad8b4ce153a9129e2857957346ca9c47
rabiulahsan <ra.rabiulahsan@gmail.com>
2024-01-24 19:02:12 +0600

    left side completed

--- src/Components/Card/DailyCard/DailyCard.jsx
+++ src/Components/Card/DailyCard/DailyCard.jsx
@@ -2,14 +2,16 @@ import dayjs from "dayjs";
 
 /* eslint-disable react/prop-types */
 const DailyCard = ({ day }) => {
-  const { conditions, datetime, tempmax, tempmin } = day;
+  const { conditions, datetime, tempmax, tempmin, icon } = day;
+  console.log(icon);
 
   const date = dayjs(datetime).format("ddd");
 
   return (
-    <div className="border-b-[.4px] border-b-gray-400 dark:border-b-slate-500 flex justify-between items-center py-6">
+    <div className="border-b-[.4px] border-b-gray-400 dark:border-b-slate-500 flex justify-between items-center py-5">
       <p className="text-gray-600 dark:text-gray-300 text-xs">{date}</p>
-      <p className="text-gray-800 dark:text-white font-semibold text-sm">
+      <p className="text-slate-700 dark:text-white font-semibold text-sm flex items-center gap-x-2">
+        <img src={`${icon}.png`} alt="" className="w-[30px]" />
         {conditions}
       </p>
       <p
